26a995d0664ac0493fa8ec89b8908bff59ade239
Untitled26 <cr3at0r0fw0r1d5@gmail.com>
2024-11-25 15:49:37 -0500

    Update PController.cs

--- Assets/Scripts/PController.cs
+++ Assets/Scripts/PController.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class PController : MonoBehaviour
 {
+    // PLAYER INPUT MANAGEMENT
+    public Camera myCam;
+    Vector3 inputDir;
+
+    public enum pStates
+    {
+        GROUNDED,
+        INAIR
+    }
+    public pStates state;
+
+    // PLAYER STAT VARIABLES
+    public float health;
+    public float speed; 
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +29,24 @@ public class PController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space)) { jump = true; }
     }
+
+    private void FixedUpdate()
+    {
+        // SYSTEM STATE
+        switch (state)
+        {
+            case pStates.GROUNDED:
+
+                break;
+
+            case pStates.INAIR:
+
+                break;
+        }
+    }
+
+
+
 }
